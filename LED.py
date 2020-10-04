4d32742e48971f86816ca9ac10c4d527b58f1705
tetsu5555 <tetete1118@gmail.com>
2020-10-04 11:04:08 +0900

    flicker LED

--- LED.py
+++ LED.py
@@ -2,14 +2,16 @@ import RPi.GPIO as GPIO
 import time
 
 LedGpio = 18
-WaitTime = 1
+WaitTime = 0.5
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LedGpio, GPIO.OUT)
-GPIO.setwarnings(False)
 
-GPIO.output(LedGpio, True)
-time.sleep(WaitTime)
-GPIO.output(LedGpio, False)
+for i in range(20):
+    GPIO.output(LedGpio, True)
+    time.sleep(WaitTime)
+    GPIO.output(LedGpio, False)
+    time.sleep(WaitTime)
 
 GPIO.cleanup()
