57c14ed53d47073f2d86b8cbe49377dd9075f51d
tetsu5555 <tetete1118@gmail.com>
2020-10-11 16:05:53 +0900

    自然の風

--- motor.py
+++ motor.py
@@ -2,12 +2,15 @@ import RPi.GPIO as GPIO
 import time
 
 motorGpio = 25 # この変数を変えることでモーターを逆回転させる
+waitTime = 1
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(motorGpio, GPIO.OUT)
 
-GPIO.output(motorGpio, True)
-time.sleep(1)
-GPIO.output(motorGpio, False)
+while True:
+    GPIO.output(motorGpio, True)
+    time.sleep(waitTime)
+    GPIO.output(motorGpio, False)
+    time.sleep(waitTime)
 
 GPIO.cleanup() # 使っていたGPIOの信号をすべて止める
