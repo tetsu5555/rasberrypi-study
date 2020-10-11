8be8d7bc1e12e84996d0dfd6ca72a48191b3bfd6
tetsu5555 <tetete1118@gmail.com>
2020-10-11 16:01:13 +0900

    モーターを逆回転させる

--- motor.py
+++ motor.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
-motorGpio = 24
+motorGpio = 25 # この変数を変えることでモーターを逆回転させる
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(motorGpio, GPIO.OUT)
@@ -10,4 +10,4 @@ GPIO.output(motorGpio, True)
 time.sleep(1)
 GPIO.output(motorGpio, False)
 
-GPIO.cleanup() // 使っていたすべてのGPIOの信号をすべて止める
+GPIO.cleanup() # 使っていたGPIOの信号をすべて止める
