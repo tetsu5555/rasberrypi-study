7ba4a2d056bd2f85c248d7c4bdc97e26bb7de45f
tetsu5555 <tetete1118@gmail.com>
2020-10-11 15:59:14 +0900

    add comment

--- motor.py
+++ motor.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 
 motorGpio = 24
 
@@ -6,3 +7,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(motorGpio, GPIO.OUT)
 
 GPIO.output(motorGpio, True)
+time.sleep(1)
+GPIO.output(motorGpio, False)
+
+GPIO.cleanup() // 使っていたすべてのGPIOの信号をすべて止める
