c6baa1a20b069e169165b8113897e99463139909
tetsu5555 <tetete1118@gmail.com>
2020-10-11 16:10:20 +0900

    簡単に方向を指定できるように

--- motor.py
+++ motor.py
@@ -1,16 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 
-motorGpio = 25 # この変数を変えることでモーターを逆回転させる
+motorGpio = [24, 25] # この変数を変えることでモーターを逆回転させる
+directRot = 0 # 回転方向を指定するための変数
 waitTime = 1
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(motorGpio, GPIO.OUT)
+for setBcm in motorGpio:
+    GPIO.setup(setBcm, GPIO.OUT)
 
 while True:
-    GPIO.output(motorGpio, True)
+    GPIO.output(motorGpio[directRot], True)
     time.sleep(waitTime)
-    GPIO.output(motorGpio, False)
+    GPIO.output(motorGpio[directRot], False)
     time.sleep(waitTime)
 
 GPIO.cleanup() # 使っていたGPIOの信号をすべて止める
