bd2ace0bd4de223df88d2e5b618324e3dea7aeac
tetsu5555 <tetete1118@gmail.com>
2020-10-11 16:18:03 +0900

    モーターを順番に正回転、逆回転させる

--- motor.py
+++ motor.py
@@ -9,10 +9,15 @@ GPIO.setmode(GPIO.BCM)
 for setBcm in motorGpio:
     GPIO.setup(setBcm, GPIO.OUT)
 
+print('hogehoge')
+
 while True:
-    GPIO.output(motorGpio[directRot], True)
-    time.sleep(waitTime)
-    GPIO.output(motorGpio[directRot], False)
-    time.sleep(waitTime)
+    for directRot in range(len(motorGpio)):
+        GPIO.output(motorGpio[directRot], True)
+        time.sleep(waitTime)
+        print(motorGpio[directRot])
+        GPIO.output(motorGpio[directRot], False)
+        time.sleep(waitTime)
+        print(motorGpio[directRot])
 
 GPIO.cleanup() # 使っていたGPIOの信号をすべて止める
